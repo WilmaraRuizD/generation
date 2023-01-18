300d72969e1835fff2e469e6bc2c8948d882dda8
WilmaraRuiz <wilmara_andreina93@hotmail.com>
2023-01-18 12:13:52 -0500

    course folder junior full/stack java

---
+++ javascript/class1/app.js
@@ -0,0 +1,55 @@
+alert('Hola mundo');
+
+//Datos en javascript 
+//declaracion en una sola lineas
+let name = 'Wilmara ruiz';
+
+//declaracion en dos lineas
+let lastName;
+lastName ='Lopez';//string 
+
+
+//numero 
+let yearOld=21;
+//No permite cambiar la variable 
+const NAME1='pepito';
+
+console.log(NAME1);
+console.log(name);
+//Console.log(lastName);
+
+//forma de camelCase
+const yearOldByAndre=20;
+const TIENE_QUE_ASI=30;
+//camelCase
+let yearOldByMara=21;
+//snake_case 
+let year_old_by_mara =20;
+let YearOldByMara=21;
+
+
+//Esto no puede ser 
+
+/* no se puede usar  solo se usa el signo de peso*/ 
+
+/*
+let $valor =23;
+let 1name="no permitido";
+let #qweqwe='No permitido';
+let -asdas='No permitido ';
+let no espacios='No Permitidos';
+
+//Lista de palabras privadas 
+// httpd://developer.mozilla.org7es/docs/Web/JavaScript/Reference/Lexical_grammar 
+let var='No permitido '
+*/
+//ejemplo de usar  
+let isDev=true;
+
+console.log(typeof name);//CARACTER
+console.log(typeof NaN);//Number
+console.log(typeof null); //OBJETO dato compuesto 
+console.log(typeof isDev)//booleano 
+
+
+
